0ee344027fd15e7f8467c591d00c6f4772379ce1
2012952877 <2012952877@qq.com>
2026-04-20 09:43:33 +0000

    漏了时区

--- src/order_cleanup.py
+++ src/order_cleanup.py
@@ -1,4 +1,3 @@
-# TODO: User A, please implement the cleanup logic here.
 # 文件路径: order_cleanup.py
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
@@ -9,8 +8,7 @@ def clean_orders():
     print("开始执行跨境物流订单清洗任务...")
     
     # 业务逻辑模拟：加载源数据
-    # 此时 User A 并没有意识到他漏掉了时区声明：
-    # spark.conf.set("spark.sql.session.timeZone", "America/Mexico_City")
+    # 此时 User A 并没有意识到他漏掉了时区声明
     
     # 模拟创建了一个清洗后的 DataFrame
     data = [("MX-1001", "Delivered"), ("MX-1002", "Pending")]
